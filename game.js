380b2b2d2880c4da2df3396474a98738d0b7bebb
goofygooeber <kyeslave2@gmail.com>
2026-09-04 10:17:57 -0700

    Update game.js

--- game.js
+++ game.js
@@ -100,9 +100,10 @@ async function getRandomLocation() {
 
   secretLat = lat;
   secretLng = lng;
-  if (Math.random() < 0.2){
-  secretLat = 37.2408785;
-  secretLng -121.9278765;
+  if (Math.random() > 0.8){
+    secretLat = 37.2408785;
+    secretLng -121.9278765;
+    alert("trevor");
   }
 
   url = `https://www.google.com/maps/embed/v1/streetview?location=${secretLat},${secretLng}&key=AIzaSyA_t5gb2Mn37dZjhsaJ4F-OPp1PWDxqZyI&fov=100&language=en&heading=307`;
